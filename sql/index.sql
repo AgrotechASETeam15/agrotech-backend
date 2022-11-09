38b3d623cdc83817ac9453ebcf59967869c8fc77
rahulbhadja <rahulbhadja@gmail.com>
2022-11-09 17:37:12 -0500

    add: pesticides table

--- sql/index.sql
+++ sql/index.sql
@@ -34,3 +34,15 @@ CREATE TABLE IF NOT EXISTS  drip_info (
         PRIMARY KEY (id),
         UNIQUE (kit_id)
     ) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS pestricides (
+        id INT NOT NULL AUTO_INCREMENT,
+        kit_id VARCHAR(255) NOT NULL,
+        kit_name VARCHAR(255) NOT NULL,
+        kit_status VARCHAR(255) NOT NULL,
+        sensor_one VARCHAR(255) NOT NULL,
+        sensor_two VARCHAR(255) NOT NULL,
+        valve_one VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE (kit_id)
+    ) ENGINE=INNODB;
