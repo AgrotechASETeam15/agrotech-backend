0b577ce63b67aa3fe91c982c71aa3caec6e6e0ac
rahulbhadja <rahulbhadja@gmail.com>
2022-11-15 10:46:08 -0500

    add: db cred

--- src/services/config/database.js
+++ src/services/config/database.js
@@ -5,13 +5,17 @@ let pool;
 async function getConnection() {
   if (!pool) {
     pool = mariaDB.createPool({
-      // host: process.env.DB_HOST || 'host.docker.internal',
-      host: process.env.DB_HOST || 'p-q.h.filess.io',
-      user: process.env.DB_USER || 'mariadb_gasolinedo',
-      password:
-        process.env.DB_PASSWORD || 'fdce3c0e3cefd4e85e5a83b98508452312f4a30e',
-      database: process.env.DB_NAME || 'mariadb_gasolinedo',
-      port: process.env.DB_PORT || 3305,
+      host: process.env.DB_HOST || 'host.docker.internal',
+      // host: process.env.DB_HOST || 'p-q.h.filess.io',
+      // user: process.env.DB_USER || 'mariadb_gasolinedo',
+      user: process.env.DB_USER || 'root',
+      // password:
+      //   process.env.DB_PASSWORD || 'fdce3c0e3cefd4e85e5a83b98508452312f4a30e',
+      password: process.env.DB_PASSWORD || 'password',
+      // database: process.env.DB_NAME || 'mariadb_gasolinedo',
+      database: process.env.DB_NAME || 'agrotech',
+      //
+      // port: process.env.DB_PORT || 3305,
       // connectionLimit: 10,
       dateStrings: true,
       decimalAsNumber: false,
